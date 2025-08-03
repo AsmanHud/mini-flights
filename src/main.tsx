64a92b7441d79bdfdbd37e3e077185092200fcc4
AsmanPooon <100230676+AsmanHud@users.noreply.github.com>
2025-08-03 18:59:03 +0500

    remove the default vite app, add a home page with nothing

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./index.css";
 import App from "./App.tsx";
 
-// Create a client
+// Create a react query client
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
